7dccf83b158864e405bd9b5852f2321298c11158
Marcelo Amorim <marcelo.med.amorim@gmail.com>
2020-08-25 02:09:38 -0300

    Adiciona estrutura inicial das classes Model

--- postgram/src/main/java/br/com/postgram/models/Post.java
+++ postgram/src/main/java/br/com/postgram/models/Post.java
@@ -1,5 +1,35 @@
 package br.com.postgram.models;
 
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
 public class Post {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(min=5, max=300)
+	private String description;
+	
+	@OneToMany(mappedBy = "comment")
+	private List<Comment> comments = new ArrayList<>();
+	
+	@NotNull
+	private OffsetDateTime created_at;
+	
 
 }
